4f153c020ed28ed23dc61ac54e11a45789d3c13d
ARod03 <ardgz2003@gmail.com>
2026-05-07 13:23:33 -0500

    Tested name formatter

--- src/main/java/com/pluralsight/NameParser.java
+++ src/main/java/com/pluralsight/NameParser.java
@@ -2,10 +2,42 @@ package com.pluralsight;
 
 public class NameParser {
 
-    public static void main(String[] args) {
+    //private constructor that will not allow me to create an instance to the class
+    //essential to keeping the class static
+    private NameParser(){}
 
-
-
+    //this is the first format method that only deals with a first and last name
+    public static String format(String firstName, String lastName) {
+        //if the input was adrian rodriguez the output should be "rodriguez, adrian"
+        return lastName + ", " + firstName;
     }
+
+    public static String format2(String prefix, String firstName, String middleName, String lastName, String suffix) {
+
+        //expected format LastName, Prefix FirstName MiddleName, Suffix
+        StringBuilder result = new StringBuilder();
+
+        //let's just handle the last name because we always get it
+        result.append(lastName).append(", ");
+
+        //if the prefix is not empty, let's include it
+        if(!prefix.isEmpty()){
+            result.append(prefix).append(" ");
+        }
+
+        result.append(firstName);
+
+        if(!middleName.isEmpty()){
+            result.append(" ").append(middleName);
+        }
+
+        if(!suffix.isEmpty()){
+            result.append(", ").append(suffix);
+        }
+
+        //return the string from the stringbuilder
+        return result.toString();
+    }
+
 
 }
---
+++ src/test/java/com/pluralsight/NameParserTest.java
@@ -0,0 +1,31 @@
+package com.pluralsight;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NameParserTest {
+
+    @Test
+    public void format_firstAndLast_shouldReturnLastCommaFirst() {
+
+        //arrange
+        String result = NameParser.format("adrian", "rodriguez");
+        String expected = "rodriguez, adrian";
+
+        //assertion
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void format_AllPieces_should_return_AllPiecesInProperFormat() {
+
+        //arrange
+        String result = NameParser.format2("Mr.", "Adrian", "O", "Rodriguez", "PHD");
+        String expected = "Rodriguez, Mr. Adrian O., PHD";
+
+        //assertion
+        assertEquals(expected, result);
+    }
+
+}
